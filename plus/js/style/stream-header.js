d90cf2e4a79759b03a0d6d334a35a418a04e5a0e
Anthony Bryant <antjbryant@gmail.com>
2011-09-03 16:35:34 +0100

    Add JS styles for the stream header

---
+++ plus/js/style/stream-header.js
@@ -0,0 +1,53 @@
+var styles =
+[
+  // Header container for the stream page
+  {
+    oldStyles:
+    [
+      "margin-top: 16px;",
+      "margin-right: 21px;",
+      "margin-bottom: 0px;",
+      "margin-left: 21px;"
+    ],
+    addedStyle:
+      "margin: 0 !important;" +
+      "display: inline-block;" +
+      "padding: 5px 0 0 6px;"
+  },
+
+  // Container for all posts
+  {
+    oldStyles:
+    [
+      "height: 100%;",
+      "position: relative;",
+      "padding-top: 15px;",
+      "width: 574px;",
+      "word-wrap: break-word;"
+    ],
+    addedStyle:
+      "padding-top: 6px !important;"
+  },
+
+  // Container for the "Share what's new..." box
+  {
+    oldStyles:
+    [
+      "margin-left: 21px;",
+      "margin-right: 21px;",
+      "margin-top: 10px;",
+      "width: 532px;"
+    ],
+    addedStyle:
+      "margin-left: 10px !important;" +
+      "margin-right: 10px !important;" +
+      "margin-top: 5px !important;" +
+      "width: auto !important;"
+  }
+]
+
+for (i in styles)
+{
+  writeStyles(styles[i].oldStyles, styles[i].addedStyle);
+}
+
