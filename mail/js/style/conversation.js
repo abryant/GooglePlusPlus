83f0a049a949490e256c2da4440bb177e338eb54
Anthony Bryant <antjbryant@gmail.com>
2011-11-04 19:15:45 +0000

    Decrease padding around hidden emails in a conversation

---
+++ mail/js/style/conversation.js
@@ -0,0 +1,24 @@
+restyles.push(
+[
+  // Remove the top-padding from historical conversation elements
+  {
+    oldStyles:
+    [
+      "{ padding-top: 12px; }"
+    ],
+    addedStyle:
+      "{ padding-top: 3px !important; }"
+  },
+
+  // Remove the bottom-padding from historical conversation elements
+  {
+    oldStyles:
+    [
+      "{ padding-bottom: 12px; }"
+    ],
+    addedStyle:
+      "{ padding-bottom: 3px !important; }"
+  }
+]
+);
+
