4e6761ec5162e86b5c47f0f38a76133c7fe9f30f
Anthony Bryant <antjbryant@gmail.com>
2011-11-04 17:59:09 +0000

    Fix the positioning of bits of the search dialog

--- mail/js/style/components.js
+++ mail/js/style/components.js
@@ -20,6 +20,45 @@ restyles.push(
       "height: 20px !important; " +
       "line-height: 20px !important; " +
       "padding: 0 3px !important;"
+  },
+
+  // Fix the positioning of the magnifying glass icon in the search dialog
+  {
+    oldStyles:
+    [
+      "background-image: url(https://mail.google.com/mail/u/0/pimages/2/search_white.png); ",
+      "height: 20px; ",
+      "left: 17px; ",
+      "position: relative; ",
+      "top: 5px; ",
+      "width: 20px;"
+    ],
+    addedStyle:
+      "top: 1px !important;"
+  },
+
+  // Fix the padding on buttons in the search dialog
+  {
+    oldStyles:
+    [
+      "overflow-x: hidden; ", "overflow-y: hidden; ",
+      "position: relative; ",
+      "padding-top: 0px; ", "padding-right: 18px; ", "padding-bottom: 0px; ", "padding-left: 6px;"
+    ],
+    addedStyle:
+      "padding: 0 14px 0 4px !important;"
+  },
+
+  // Fix the positioning of the drop down arrows on the buttons in the search dialog
+  {
+    oldStyles:
+    [
+      "position: absolute; ",
+      "right: 5px; ",
+      "top: 12px;"
+    ],
+    addedStyle:
+      "top: 9px !important;"
   }
 ]
 );
