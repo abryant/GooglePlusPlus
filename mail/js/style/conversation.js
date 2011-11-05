eb88cb7b8bff549ac956d71a9875d3fe8c6749c9
Anthony Bryant <antjbryant@gmail.com>
2011-11-05 05:21:09 +0000

    Decrease some heights in the conversation view

--- mail/js/style/conversation.js
+++ mail/js/style/conversation.js
@@ -18,6 +18,32 @@ restyles.push(
     ],
     addedStyle:
       "{ padding-bottom: 3px !important; }"
+  },
+
+  // Decrease the top-padding of the picture to the left of a conversation element
+  {
+    oldStyles:
+    [
+      "cursor: pointer; ",
+      "float: left; ",
+      "padding-top: 12px; ", "padding-right: 12px; ", "padding-bottom: 0px; ", "padding-left: 0px;"
+    ],
+    addedStyle:
+      "padding-top: 3px !important;"
+  },
+
+  // Decrease the height of the star to the right hand side of a conversation element
+  {
+    oldStyles:
+    [
+      "display: inline-block; ",
+      "height: 19px; ",
+      "text-align: center; ",
+      "width: 19px; ",
+      "padding-top: 2px; ", "padding-right: 2px; ", "padding-bottom: 2px; ", "padding-left: 2px;"
+    ],
+    addedStyle:
+      "height: 16px !important;"
   }
 ]
 );
